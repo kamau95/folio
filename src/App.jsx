b405d6210d6fb0552693a4444b318399ab036e22
linus kiogora <linuskamau586@gmail.com>
2023-04-08 23:53:53 +0300

    a commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+import './App.css'
+
+function App( ) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonials/>
+    <Contact/>
+    <Footer/>
+    </>
+      
+    
+    </div>
+  )
+}
+
+export default App
